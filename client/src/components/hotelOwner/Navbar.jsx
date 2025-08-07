7c074b4b1bc979ea9ab7991fa19e084c400c83f8
Soumitri Mishra <soumitrimishra1@gmail.com>
2025-08-07 22:15:29 +0530

    project complete

--- client/src/components/hotelOwner/Navbar.jsx
+++ client/src/components/hotelOwner/Navbar.jsx
@@ -1,27 +1,15 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { assets } from "../../assets/assets";
 import { UserButton } from "@clerk/clerk-react";
 import { Link } from "react-router-dom";
-import "@theme-toggles/react/css/Expand.css";
-import { Expand } from "@theme-toggles/react";
-import { useLocalStorage } from "usehooks-ts";
+import ThemeToggle from "../../components/ThemeToggle"; // Adjust the path if needed
 
 const Navbar = ({ switchTheme }) => {
-  // for toggle btn
-  const [isToggled, setToggle] = useLocalStorage("theme-toggle", false);
-  useEffect(() => {
-    if (isToggled) {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
-  }, [isToggled]);
-
   return (
     <div
       className="flex items-center justify-between px-4 md:px-8
-    border-b border-gray-200/60 py-3 text-[var(--black-one)] bg-[var(--white-one)] transition-all
-    duration-300"
+      border-b border-gray-200/60 py-3 text-[var(--black-one)] bg-[var(--white-one)] transition-all
+      duration-300"
     >
       <Link to="/">
         <img
@@ -32,15 +20,11 @@ const Navbar = ({ switchTheme }) => {
       </Link>
 
       <div className="flex justify-center items-center">
-        <div onClick={switchTheme} className="cursor-pointer mr-5 mt-1">
-          <Expand
-            duration={750}
-            className="text-xl"
-            toggled={isToggled}
-            toggle={() => setToggle(!isToggled)}
-            style={{ color: isToggled ? "black" : "white" }}
-          />
+        {/* Use your custom ThemeToggle */}
+        <div className="cursor-pointer mr-5 mt-1">
+          <ThemeToggle switchTheme={switchTheme} />
         </div>
+
         <UserButton />
       </div>
     </div>
